c6b7fac5cc6e5fa750351b94d4c437c0cd13b074
Ubong Sylvester <ubongsly@gmail.com>
2023-02-10 12:14:31 +0100

    RoadmapTabs component tested 2

--- src/__tests__/components/roadmap/RoadmapTabs.test.js
+++ src/__tests__/components/roadmap/RoadmapTabs.test.js
@@ -1,5 +1,7 @@
 import RoadmapTabs from '../../../components/roadmap/RoadmapTabs';
+import { colors } from '../../../styles';
 import { render, screen } from '../../../test-utils';
+import user from '@testing-library/user-event';
 
 describe('RoadmapTabs', () => {
   test('should render correctly', () => {
@@ -25,5 +27,41 @@ describe('RoadmapTabs', () => {
       name: /in-progress/i,
     });
     expect(inProgressButton).toBeInTheDocument();
+  });
+  test('should check if active have the right styles', async () => {
+    render(<RoadmapTabs />);
+
+    const plannedButton = screen.getByRole('button', {
+      name: /planned/i,
+    });
+    const liveButton = screen.getByRole('button', {
+      name: /live/i,
+    });
+    const inProgressButton = screen.getByRole('button', {
+      name: /in-progress/i,
+    });
+
+    expect(plannedButton).toHaveStyle({
+      'border-bottom': `4px solid ${colors.orange}`,
+    });
+    expect(plannedButton).toHaveClass('active');
+    expect(liveButton).not.toHaveClass('active');
+    expect(inProgressButton).not.toHaveClass('active');
+
+    await user.click(liveButton);
+    expect(liveButton).toHaveStyle({
+      'border-bottom': `4px solid ${colors.blue}`,
+    });
+    expect(liveButton).toHaveClass('active');
+    expect(plannedButton).not.toHaveClass('active');
+    expect(inProgressButton).not.toHaveClass('active');
+
+    await user.click(inProgressButton);
+    expect(inProgressButton).toHaveStyle({
+      'border-bottom': `4px solid ${colors.purple}`,
+    });
+    expect(inProgressButton).toHaveClass('active');
+    expect(liveButton).not.toHaveClass('active');
+    expect(plannedButton).not.toHaveClass('active');
   });
 });
